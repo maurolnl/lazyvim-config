1a002930d1f3ec869f2d56c137000f3e4a75975c
maurolnl <mauroleoquiroga@gmail.com>
2025-11-10 22:56:06 -0300

    add theme for macos

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,1 +1,30 @@
-/home/maurolnl/.config/omarchy/current/theme/neovim.lua
+return {
+  -- Configure LazyVim to use your chosen colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+  -- Load and configure the active theme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      transparent_background = false,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+        },
+      },
+    },
+  },
+}
